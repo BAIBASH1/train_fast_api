d035fd6602f751478a48501be78dd1b5040f9529
BAIBASH1 <gazinyr2014@mail.ru>
2025-02-25 12:14:51 +0300

    simplified add method in booking_dao.py

--- app/booking/booking_dao.py
+++ app/booking/booking_dao.py
@@ -10,6 +10,7 @@ from datetime import date
 
 from sqlalchemy import and_, func, insert, select
 from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.booking.booking_models import Bookings
 from app.booking.booking_schemas import BookingsInfoSchema
@@ -62,53 +63,28 @@ class BookingDAO(BaseDAO):
             The function does not propagate these exceptions further.
         """
         try:
-            booked_rooms = (
-                select(Bookings)
-                .where(
-                    and_(
-                        Bookings.room_id == room_id,
-                        Bookings.date_from <= date_to,
-                        Bookings.date_to >= date_from,
-                    )
-                )
-                .cte("booked_rooms")
-            )
-
-            query_rooms_left = (
-                select((Rooms.quantity - func.count(booked_rooms.c.room_id)).label("rooms_left"))
-                .select_from(Rooms)
-                .join(
-                    booked_rooms,
-                    Rooms.id == booked_rooms.c.room_id,
-                    isouter=True,
-                )
-                .where(room_id == Rooms.id)
-                .group_by(Rooms.quantity, booked_rooms.c.room_id)
-            )
-
             async with async_session_maker() as session:
-                rooms_left = await session.execute(query_rooms_left)
-                rooms_left: int = rooms_left.scalar()
-                if rooms_left > 0:
-                    query_get_price = select(Rooms.price).filter_by(id=room_id)
-                    price = await session.execute(query_get_price)
-                    price: int = price.scalar()
-                    add_booking_query = (
-                        insert(Bookings)
-                        .values(
-                            room_id=room_id,
-                            user_id=user_id,
-                            date_from=date_from,
-                            date_to=date_to,
-                            price=price,
-                        )
-                        .returning(Bookings)
+                available_rooms = await cls._get_available_rooms(
+                    session, room_id, date_from, date_to
+                )
+                if available_rooms <= 0:
+                    logger.info(
+                        "Нет свободных комнат для бронирования.",
+                        extra={"room_id": room_id, "date_from": date_from, "date_to": date_to},
                     )
-                    booking_new = await session.execute(add_booking_query)
-                    await session.commit()
-                    return booking_new.scalar()
-                else:
                     return None
+                price: int | None = await cls._get_room_price(session, room_id)
+
+                booking_new = await cls._create_booking(
+                    session,
+                    user_id,
+                    room_id,
+                    date_from,
+                    date_to,
+                    price,
+                )
+                await session.commit()
+                return booking_new
         except (SQLAlchemyError, Exception) as error:
             if isinstance(error, SQLAlchemyError):
                 message = "Database Exc"
@@ -167,3 +143,98 @@ class BookingDAO(BaseDAO):
             bookings = await session.execute(query_bookings)
             bookings = bookings.mappings().all()
             return bookings
+
+    @classmethod
+    async def _get_available_rooms(
+        cls, session: AsyncSession, room_id: int, date_from: date, date_to: date
+    ) -> int:
+        """
+        return available rooms for the specified date range.
+
+        Args:
+            session (AsyncSession): The database session object.
+            room_id (int): The identifier of the room to be booked.
+            date_from (date): The starting date of the booking period.
+            date_to (date): The ending date of the booking period.
+        Returns:
+            int: The number of available rooms for the specified date range.
+        """
+        booked_rooms = (
+            select(Bookings)
+            .where(
+                and_(
+                    Bookings.room_id == room_id,
+                    Bookings.date_from <= date_to,
+                    Bookings.date_to >= date_from,
+                )
+            )
+            .cte("booked_rooms")
+        )
+
+        query_rooms_left = (
+            select((Rooms.quantity - func.count(booked_rooms.c.room_id)).label("rooms_left"))
+            .select_from(Rooms)
+            .join(
+                booked_rooms,
+                Rooms.id == booked_rooms.c.room_id,
+                isouter=True,
+            )
+            .where(room_id == Rooms.id)
+            .group_by(Rooms.quantity, booked_rooms.c.room_id)
+        )
+        rooms_result = await session.execute(query_rooms_left)
+        rooms_left = rooms_result.scalar()
+        return rooms_left if rooms_left is not None else 0
+
+    @classmethod
+    async def _get_room_price(cls, session: AsyncSession, room_id: int) -> int | None:
+        """
+        return rooms price for specified room.
+
+        Args:
+            session (AsyncSession): The database session object.
+            room_id (int): The identifier of the room.
+        Return:
+            int | None: Room price.
+        """
+        query = select(Rooms.price).filter_by(id=room_id)
+        result = await session.execute(query)
+        return result.scalar()
+
+    @classmethod
+    async def _create_booking(
+        cls,
+        session,
+        user_id: int,
+        room_id: int,
+        date_from: date,
+        date_to: date,
+        price: int,
+    ) -> Bookings:
+        """
+        create a new record of booking with specified params
+
+        Args:
+            session (AsyncSession): The database session object.
+            user_id (int): The identifier of the user making the booking.
+            room_id (int): The identifier of the room to be booked.
+            date_from (date): The starting date of the booking period.
+            date_to (date): The ending date of the booking period.
+            price (int): The price of the booking.
+
+        Returns:
+            Bookings: The newly created booking object if the booking is successful.
+        """
+        add_booking_query = (
+            insert(Bookings)
+            .values(
+                room_id=room_id,
+                user_id=user_id,
+                date_from=date_from,
+                date_to=date_to,
+                price=price,
+            )
+            .returning(Bookings)
+        )
+        result = await session.execute(add_booking_query)
+        return result.scalar()
